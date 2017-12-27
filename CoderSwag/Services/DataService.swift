738cb03451a1755241ed2b00681d99b309a4b31c
Joy Umali <joyumali@gmail.com>
2017-12-26 23:44:44 -0800

    added products to DataService

--- CoderSwag/Services/DataService.swift
+++ CoderSwag/Services/DataService.swift
@@ -19,8 +19,48 @@ class DataService { // This is a singleton.  Only want one copy of a data servic
         Category(title: "DIGITAL", imageName: "digital.png")
     ]
     
+    private let hats = [
+        Product(title: "Devslopes Logo Graphic Beanie", imageName: "hat01.png", price: "$18"),
+        Product(title: "Devslopes Logo Hat Black", imageName: "hat02.png", price: "$20"),
+        Product(title: "Devslopes Logo Hat White", imageName: "hat03.png", price: "$20"),
+        Product(title: "Devslopes Logo Hat Snapback", imageName: "hat04.png", price: "$20")
+    ]
+    
+    private let hoodies = [
+        Product(title: "Devslopes Logo Hoodie Grey", imageName: "hoodie01.png", price: "$32"),
+        Product(title: "Devslopes Logo Hoodie Red", imageName: "hoodie02.png", price: "$32"),
+        Product(title: "Devslopes Hoodie Grey", imageName: "hoodie03.png", price: "$32"),
+        Product(title: "Devslopes Hoodie Black", imageName: "hoodie04.png", price: "$32")
+    ]
+    
+    private let shirts = [
+        Product(title: "Devslopes Logo Shirt Black", imageName: "shirt01.png", price: "$18"),
+        Product(title: "Devslopes Badge Shirt Light Grey", imageName: "shirt02.png", price: "$18"),
+        Product(title: "Devslopes Logo Shirt Black", imageName: "shirt03.png", price: "$18"),
+        Product(title: "Devslopes Logo Shirt Black", imageName: "shirt04.png", price: "$18")
+    ]
+    
     // asks server to get the categories
     func getCategories() -> [Category] {
         return categories
     }
+    
+    func gethats() -> [Product] {
+        return hats
+    }
+    
+    func gethoodies() -> [Product] {
+        return hoodies
+    }
+    
+    func getShirts() -> [Product] {
+        return shirts
+    }
+
 }
+
+
+
+
+
+
